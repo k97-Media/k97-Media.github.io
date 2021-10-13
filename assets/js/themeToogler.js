e90723f7a6e318fe54c6c13be808bd9f4c403778
Korak Kurani <kurani@comcard.de>
2021-10-13 08:42:01 +0200

    add themeToogler

---
+++ assets/js/themeToogler.js
@@ -0,0 +1,32 @@
+const icon = document.getElementById("theme-toogler")
+const logo = document.getElementById("logo")
+const socialLinks = document.querySelectorAll(".social-link")
+let darkTheme = false
+body.style.transition = "All .5s ease-in-out"
+
+
+const toogleDark = () => {
+  body.classList.add("dark-mode")
+  socialLinks.forEach((link)=>link.classList.add("dark-mode-link"))
+  logo.classList.add("dark-mode-logo")
+  icon.firstChild.setAttribute("name", "sunny-outline")
+}
+
+const toogleLight = () =>{
+  body.classList.remove("dark-mode")
+  socialLinks.forEach((link)=>link.classList.remove("dark-mode-link"))
+  logo.classList.remove("dark-mode-logo")
+  icon.firstChild.setAttribute("name", "moon-outline")
+}
+
+
+icon.addEventListener("click", () => {
+  darkTheme = !darkTheme
+  localStorage.setItem("theme", `${darkTheme? "dark" : "light"}`)
+  localStorage.getItem("theme") == "dark" ? toogleDark() : toogleLight()
+})
+
+
+localStorage.getItem("theme") == "dark" ? toogleDark() : toogleLight()
+
+
