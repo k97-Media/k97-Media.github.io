2e797642c44cfe7a62de38808c20097df8d2ff9e
Dani Linux-lp <dani.linux.97@gmail.com>
2021-11-07 23:12:47 +0100

    Fix null error

--- assets/js/themeToogler.js
+++ assets/js/themeToogler.js
@@ -1,32 +1,35 @@
-const toogler = document.getElementById("toogle")
-const logo = document.getElementById("logo")
-const socialLinks = document.querySelectorAll(".social-link")
-let darkTheme = false
-body.style.transition = "All .5s ease-in-out"
-
+const toogler = document.getElementById("toogle");
+const logo = document.getElementById("logo");
+const socialLinks = document.querySelectorAll(".social-link");
+let darkTheme = false;
+body.style.transition = "All .5s ease-in-out";
 
 const toogleDark = () => {
-  body.classList.add("dark-mode")
-  socialLinks.forEach((link)=>link.classList.add("dark-mode-link"))
-  logo.classList.add("dark-mode-logo")
-  toogler.classList.add("toogler-dark")
-}
+	body.classList.add("dark-mode");
+	if (socialLinks !== null) {
+		socialLinks.forEach((link) => link.classList.add("dark-mode-link"));
+	}
+	if (logo !== null) {
+		logo.classList.add("dark-mode-logo");
+	}
+	toogler.classList.add("toogler-dark");
+};
 
-const toogleLight = () =>{
-  body.classList.remove("dark-mode")
-  socialLinks.forEach((link)=>link.classList.remove("dark-mode-link"))
-  logo.classList.remove("dark-mode-logo")
-  toogler.classList.remove("toogler-dark")
-}
-
+const toogleLight = () => {
+	body.classList.remove("dark-mode");
+	if (socialLinks !== null) {
+		socialLinks.forEach((link) => link.classList.remove("dark-mode-link"));
+	}
+	if (logo !== null) {
+		logo.classList.remove("dark-mode-logo");
+	}
+	toogler.classList.remove("toogler-dark");
+};
 
 toogler.addEventListener("click", () => {
-  darkTheme = !darkTheme
-  localStorage.setItem("theme", `${darkTheme? "dark" : "light"}`)
-  localStorage.getItem("theme") == "dark" ? toogleDark() : toogleLight()
-})
+	darkTheme = !darkTheme;
+	localStorage.setItem("theme", `${darkTheme ? "dark" : "light"}`);
+	localStorage.getItem("theme") == "dark" ? toogleDark() : toogleLight();
+});
 
-
-localStorage.getItem("theme") == "dark" ? toogleDark() : toogleLight()
-
-
+localStorage.getItem("theme") == "dark" ? toogleDark() : toogleLight();
