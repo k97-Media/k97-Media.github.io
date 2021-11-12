feb57b187b56db7c8832928e083d3e89988de1e9
Korak Kurani <kurani@comcard.de>
2021-11-12 08:10:52 +0100

    Add better shadow

--- assets/js/themeToogler.js
+++ assets/js/themeToogler.js
@@ -6,23 +6,11 @@ body.style.transition = "All .5s ease-in-out";
 
 const toogleDark = () => {
 	body.classList.add("dark-mode");
-	if (socialLinks !== null) {
-		socialLinks.forEach((link) => link.classList.add("dark-mode-link"));
-	}
-	if (logo !== null) {
-		logo.classList.add("dark-mode-logo");
-	}
 	toogler.classList.add("toogler-dark");
 };
 
 const toogleLight = () => {
 	body.classList.remove("dark-mode");
-	if (socialLinks !== null) {
-		socialLinks.forEach((link) => link.classList.remove("dark-mode-link"));
-	}
-	if (logo !== null) {
-		logo.classList.remove("dark-mode-logo");
-	}
 	toogler.classList.remove("toogler-dark");
 };
 
