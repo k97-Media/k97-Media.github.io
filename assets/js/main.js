1e46543f67bee1d1051f2496483ea4edb8e94f45
dani-desk-lx <dani.linux.97@gmail.com>
2021-11-17 11:49:42 +0100

    pop in/out navbar and footer on scrolling

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,3 +8,32 @@ if (navLinks.children.length > 0) {
 			: link.classList.remove("current-page-link");
 	});
 }
+
+const scrollFunction = (elementScroll) => {
+	const navbar = document.querySelector(".navbar");
+	const footer = document.querySelector("footer");
+	if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+		navbar.classList.remove("pop-in");
+		navbar.classList.add("pop-out");
+	} else {
+		navbar.classList.remove("pop-out");
+		navbar.classList.add("pop-in");
+	}
+	if (
+		elementScroll.scrollHeight - elementScroll.scrollTop <=
+		elementScroll.clientHeight + 100
+	) {
+		footer.classList.remove("pop-out");
+		footer.classList.add("pop-in");
+	} else {
+		footer.classList.remove("pop-in");
+		footer.classList.add("pop-out");
+	}
+};
+
+let lastPos = 0;
+
+window.onscroll = (event) => {
+	let elementScroll = event.target.scrollingElement;
+	scrollFunction(elementScroll);
+};
